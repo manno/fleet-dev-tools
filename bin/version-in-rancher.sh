098fe1b22e9036049c3f5f20fcebeaa1b64a0b00
Mario Manno <mario.manno@suse.com>
2023-06-19 15:42:05 +0200

    versions-in-rancher.sh url

--- bin/version-in-rancher.sh
+++ bin/version-in-rancher.sh
@@ -2,6 +2,9 @@
 
 VER=${1-v2.6.7}
 
+if ! echo "$VER" | grep "^v"; then
+  VER="v$VER"
+fi
+
 echo "Rancher $VER"
-#wget https://github.com/rancher/rancher/releases/download/"$VER"/rancher-images.txt -qO- | grep -E "fleet|gitjob|tekton-utils|/kubectl"
-wget https://github.com/rancher/rancher/raw/"$VER"/package/Dockerfile -qO- | grep CATTLE_FLEET_MIN_VERSION
+wget https://github.com/rancher/rancher/releases/download/"$VER"/rancher-images.txt -qO- | grep -E "fleet|gitjob|tekton-utils|/kubectl"
