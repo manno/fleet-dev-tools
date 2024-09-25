43a5f7dd2e17f817db361e08fc1f31195ed55417
Mario Manno <mario.manno@suse.com>
2024-09-25 15:17:21 +0200

    fix bench

--- bin/bench-create.sh
+++ bin/bench-create.sh
@@ -1,21 +1,24 @@
+#!/bin/bash
+ns=fleet-local
+
 echo -n "start creating: "
 date
-time kubectl apply --wait -f gitrepo-lots.yaml
+time kubectl apply -n "$ns" --wait -f gitrepo-lots.yaml
 sleep 10
-kubectl wait --for=condition=Ready=True --timeout=2m -n fleet-default gitrepo lots-a
-kubectl wait --for=condition=Ready=True --timeout=2m -n fleet-default gitrepo lots-b
-kubectl wait --for=condition=Ready=True --timeout=2m -n fleet-default gitrepo lots-c
-kubectl wait --for=condition=Ready=True --timeout=2m -n fleet-default gitrepo lots-d
-kubectl wait --for=condition=Ready=True --timeout=2m -n fleet-default gitrepo lots-e
-kubectl wait --for=condition=Ready=True --timeout=2m -n fleet-default gitrepo lots-f
-kubectl wait --for=condition=Ready=True --timeout=2m -n fleet-default gitrepo lots-g
-kubectl wait --for=condition=Ready=True --timeout=2m -n fleet-default gitrepo lots-h
-kubectl wait --for=condition=Ready=True --timeout=2m -n fleet-default gitrepo lots-i
-kubectl wait --for=condition=Ready=True --timeout=2m -n fleet-default gitrepo lots-j
+kubectl wait --for=condition=Ready=True --timeout=2m -n "$ns" gitrepo lots-a
+kubectl wait --for=condition=Ready=True --timeout=2m -n "$ns" gitrepo lots-b
+kubectl wait --for=condition=Ready=True --timeout=2m -n "$ns" gitrepo lots-c
+kubectl wait --for=condition=Ready=True --timeout=2m -n "$ns" gitrepo lots-d
+kubectl wait --for=condition=Ready=True --timeout=2m -n "$ns" gitrepo lots-e
+kubectl wait --for=condition=Ready=True --timeout=2m -n "$ns" gitrepo lots-f
+kubectl wait --for=condition=Ready=True --timeout=2m -n "$ns" gitrepo lots-g
+kubectl wait --for=condition=Ready=True --timeout=2m -n "$ns" gitrepo lots-h
+kubectl wait --for=condition=Ready=True --timeout=2m -n "$ns" gitrepo lots-i
+kubectl wait --for=condition=Ready=True --timeout=2m -n "$ns" gitrepo lots-j
 
 # 10 repos on 10 clusters with 10 resources each
-while [ $(kubectl -n fleet-default get gitrepo -o jsonpath='{range .items[*]}{.status.display.readyBundleDeployments}{"\n"}{end}' | grep '100/100' | wc -l) -ne 10 ]; do
-  kubectl -n fleet-default get gitrepo -o jsonpath='{range .items[*]}{.status.display.readyBundleDeployments}{"\n"}{end}'
+while [ $(kubectl -n "$ns" get gitrepo -o jsonpath='{range .items[*]}{.status.display.readyBundleDeployments}{"\n"}{end}' | grep '10/10' | wc -l) -ne 10 ]; do
+  kubectl -n "$ns" get gitrepo -o jsonpath='{range .items[*]}{.status.display.readyBundleDeployments}{"\n"}{end}'
   sleep 1
 done
 
--- bin/bench-delete.sh
+++ bin/bench-delete.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+ns=fleet-local
+
 echo -n "start deleting: "
 date
 
-time kubectl delete --wait -n fleet-default gitrepo lots-a lots-b lots-c lots-d lots-e lots-f lots-g lots-h lots-i lots-j
-while [ $(kubectl -n fleet-default get clusters.fleet.cattle.io -o jsonpath='{range .items[*]}{.status.display.readyBundles}{"\n"}{end}' | grep '1/1' | wc -l) -ne 10 ]; do
+time kubectl delete --wait -n "$ns" gitrepo lots-a lots-b lots-c lots-d lots-e lots-f lots-g lots-h lots-i lots-j
+while [ $(kubectl -n "$ns" get clusters.fleet.cattle.io -o jsonpath='{range .items[*]}{.status.display.readyBundles}{"\n"}{end}' | grep '1/1' | wc -l) -ne 1 ]; do
   sleep 1
 done
 
