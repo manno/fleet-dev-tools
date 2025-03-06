e1a7e47671b0261aa4e7dee2ecd1b823fab79f7c
Mario Manno <mmanno@suse.com>
2025-03-06 14:59:33 +0100

    lost+found

--- bin/backport-pr.sh
+++ bin/backport-pr.sh
@@ -16,15 +16,22 @@ release_branch=${3-release/v0.10}
 head=${4-HEAD} #v0.10-9010-feature..
 
 if [ $# -lt 4 ]; then
-  echo "Usage: $0 issue main_pr [release_branch] [HEAD]"
-  echo "Example: $0 123 456 2.9.2 release/v0.10 HEAD"
+  echo "Usage: $0 issue main_pr release_branch feature_branch"
+  echo "Example: $0 123 456 release/v0.10 myfeature"
   exit 1
 fi
 
 version=$(echo "$release_branch" | cut -d'/' -f2)
 title=$(gh pr -R "$repo" view "$main_pr" --json title -q '.title')
 
-echo "Creating backport pr: '[$version] Backport of $title', refers to #$issue, to $release_branch, from git branch $head"
+echo "Create and push backport branch to $head, e.g. run:"
+echo "git switch $release_branch && git pull"
+echo "git switch -c $head"
+echo "git cherry-pick ..."
+echo "git push origin $head"
+read -rp "Create and push $head now. Press any key..." -n1 -s
+
+echo "Creating backport PR: '[$version] Backport of $title', refers to #$issue, to $release_branch, from git branch $head"
 read -rp "Press any key to continue... " -n1 -s
 
 gh pr create -R "$repo" --base "$release_branch" --head "$head" --title "[$version] Backport of $title" --body "Backport of #$main_pr, refers to #$issue"
