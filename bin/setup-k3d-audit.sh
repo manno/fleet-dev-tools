8dfbf40c18b368f3d5a5506c85678566ec1c0d5a
Mario Manno <mmanno@suse.com>
2025-10-23 16:45:58 +0200

    audit config for k3d

--- bin/setup-k3d-audit.sh
+++ bin/setup-k3d-audit.sh
@@ -7,30 +7,76 @@ kind: Policy
 omitStages:
 - "RequestReceived"
 rules:
-- level: Metadata
+# Don't log requests to a configmap called "controller-leader"
+- level: None
   resources:
   - group: ""
-    resources: ["pods"]
-  namespaces: ["cattle-fleet-system"]
+    resources: ["configmaps"]
+    resourceNames: ["controller-leader"]
+# Don't log authenticated requests to certain non-resource URL paths.
+- level: None
+  userGroups: ["system:authenticated"]
+  nonResourceURLs:
+  - "/api*" # Wildcard matching.
+  - "/version"
+# Don't log deployments in kube-system
 - level: None
   resources:
   - group: "apps"
     resources: ["deployments"]
   namespaces: ["kube-system"]
+# Log metadata for fleet pods
+- level: Metadata
+  resources:
+  - group: ""
+    resources: ["pods"]
+  namespaces: ["cattle-fleet-system"]
+# Log configmap and secret changes in all other namespaces at the Metadata level.
+- level: Metadata
+  resources:
+  - group: "" # core API group
+    resources: ["secrets", "configmaps"]
+# Log Rancher resources
 - level: RequestResponse
   resources:
-  - group: "apps"
-    resources: ["deployments"]
+  - group: "management.cattle.io"
+# Log Fleet resources
+- level: RequestResponse
+  resources:
+  - group: "fleet.cattle.io"
+# Don't log anything else
 - level: None
 EOF
 fi
 
+mkdir -p audit
 
-k3d cluster create upstream --servers 1 --api-port 36443  -p '80:80@server:0' -p '443:443@server:0' \
+name=${1-upstream}
+offs=${2-0}
+args=${k3d_args---network fleet}
+unique_api_port=${unique_api_port-36443}
+unique_tls_port=${unique_tls_port-443}
+
+k3d cluster create "$name" \
+  --servers 3 \
+  --api-port "$unique_api_port" \
+  -p "$(( 8080 + offs )):8080@server:0" \
+  -p "$(( 8081 + offs )):8081@server:0" \
+  -p "$(( 8082 + offs )):8082@server:0" \
+  -p "$(( 4343 + offs )):4343@server:0" \
+  -p "$unique_tls_port:443@server:0" \
+  --k3s-arg '--tls-san=k3d-upstream-server-0@server:0' \
   --k3s-arg '--kube-apiserver-arg=audit-policy-file=/var/lib/rancher/k3s/server/manifests/audit.yaml@server:*' \
   --k3s-arg '--kube-apiserver-arg=audit-log-path=/var/log/kubernetes/audit/audit.log@server:*' \
-  --volume "$(pwd)/audit.yaml:/var/lib/rancher/k3s/server/manifests/audit.yaml"
+  --volume "$(pwd)/audit.yaml:/var/lib/rancher/k3s/server/manifests/audit.yaml" \
+  --volume "$(pwd)/audit:/var/log/kubernetes/audit" \
+  $args
 
-kubectl config use-context k3d-upstream
-
+# k3d cluster create upstream --servers 1 --api-port 36443  -p '80:80@server:0' -p '443:443@server:0' \
+#   --k3s-arg '--kube-apiserver-arg=audit-policy-file=/var/lib/rancher/k3s/server/manifests/audit.yaml@server:*' \
+#   --k3s-arg '--kube-apiserver-arg=audit-log-path=/var/log/kubernetes/audit/audit.log@server:*' \
+#   --volume "$(pwd)/audit.yaml:/var/lib/rancher/k3s/server/manifests/audit.yaml"
+#
+# kubectl config use-context k3d-upstream
+#
 # docker exec k3d-upstream-server-0 tail -f /var/log/kubernetes/audit/audit.log | tee redeploy.$$
